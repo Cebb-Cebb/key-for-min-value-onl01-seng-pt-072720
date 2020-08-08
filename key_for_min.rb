967f693f925989344c3fa6a8c49f94bf505cc7cf
hersson ceballos <herssonceballos@gmail.com>
2020-08-08 22:20:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -26,12 +26,12 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       array << value
     end
-    min_num = array[0]
+    small_value = array[0]
     array.each do |value|
-      min_num = value if value < min_num
+      small_value = value if value < small_value
     end
     name_hash.each do |key, value|
-      if min_num == value
+      if small_value == value
         return key
       end
     end
